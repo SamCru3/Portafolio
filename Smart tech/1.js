4e2e71f06f2e0c87ba71766a8f8026d561685c11
SamCru3 <116518461+SamCru3@users.noreply.github.com>
2025-11-05 21:13:13 -0500

    Delete Smart tech directory

--- Smart tech/1.js
+++
@@ -1,5 +0,0 @@
-const imagen = document.querySelector(".parallax-img");
-window.addEventListener("scroll", () => {
-    const desplazamiento = window.scrollY * 0.1;
-    imagen.style.transform = `translateY(${desplazamiento}px)`;
-});
